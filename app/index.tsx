32eda4f88853b72ac77b46cdad417f300518015f
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-08-16 20:10:45 -0600

    Tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 const App = () => {
-  return <Redirect href="/(stack)/home" />;
+  return <Redirect href="/tabs" />;
   // return (
   //   <SafeAreaView>
   //     <View className="mt-6 mx-2.5">
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,30 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "green" }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home Screen",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="home-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: "Favoritos",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="star-outline" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
